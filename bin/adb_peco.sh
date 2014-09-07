4c82c8e1e27ecd277f0d2df7cf73b418c00c1a96
Tomoki Yamashita <tomoki-yamashita@cookpad.com>
2014-09-08 08:48:39 +0900

    Initial commit

---
+++ bin/adb_peco.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "command must not be empty"
+    exit
+fi
+
+if [ $# -eq 1 ]; then
+    if [ $1 = "adb" ]; then
+        $1
+        exit
+    fi
+fi
+
+count=`adb devices | sed '/^$/d' | wc -l`
+
+if [ $count -eq 1 ]; then
+	echo "device not found"
+	exit
+fi
+
+if [ $count -eq 2 ]; then
+	device=`adb devices | sed -e "1,1d"`
+else
+	device=`adb devices | sed -e "1,1d" | peco`
+fi
+
+
+if [ "$device" = "" ]; then
+    exit
+fi
+
+IFS="	" read -r id state <<< "$device"
+
+$1 -s $id ${@:2}
